3d32e98c3741a16911267f4efbe2b28e4c47ee63
EdgardoDev <ep-marketing-media@live.com>
2022-02-21 14:30:50 +0100

    Variables and control structures

--- ticketing/ticketing/views.py
+++ ticketing/ticketing/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from ticketing.models import Ticket
+import random
+import string
+
+def randomString(stringLength=8):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
 
 def index(request):
     return render(request, "index.html")
+
+def submit(request):
+    new_ticket = Ticket(submitter=randomString(), body="I need help, I've found a new bug.")
+    new_ticket.save()
+    return render(request, "submit.html")
+
+def tickets(request):
+    all_tickets = Ticket.objects.all()
+    return render(request, "tickets.html", {"tickets": all_tickets})
